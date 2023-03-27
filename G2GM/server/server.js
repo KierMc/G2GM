2a5e191545e37978fc80e3508d25eefb3a57ec2a
KierMc <kiernanmcvay@gmail.com>
2023-03-27 16:18:26 -0700

    second commit

---
+++ G2GM/server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require('cors')
+const app = express();
+const port = 8000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+require("./config/mongoose.config")
+
+app.listen(port, () => {console.log(`Welcome to the Database! You are in port: ${port}`)})
